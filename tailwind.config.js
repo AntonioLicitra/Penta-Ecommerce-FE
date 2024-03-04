f2fd28cda382180f2736eb5ade0a4b25c882d8e6
AntoninoPalmaEsposito <tonyaccacciadite@gmail.com>
2024-03-04 17:50:28 +0100

    config tail

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
-	},
+		extend: {
+		  colors: {
+			'custom-blue': '#007bff', // Esempio di colore esadecimale
+			'custom-green': 'green', // Esempio di nome di colore CSS
+			'custom-gradient': ['#ff0000', '#00ff00'], // Esempio di array di gradienti
+		  },
+		  fontSize: {
+			'enorme': '200px'
+		  },
+		},
+	  },
 	plugins: [],
 };
