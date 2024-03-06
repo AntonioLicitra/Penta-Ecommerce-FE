bef766dc838416cbccf0700d4e62741b71c8e3d1
AntonioLicitra <antolici@outlook.com>
2024-03-06 16:37:05 +0100

    pagina prodotto

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -43,7 +43,34 @@ export default {
 </script>
 
 <template>
+  
+    
+    <section v-if="product" class="flex flex-col lg:flex-row mt-10">
+        <div class="w-[100%] lg:w-[50%] gap-10 flex flex-col justify-center items-center">
+        <img class="w-[300px]" v-for="imageURL in product.imageURLs" :src="imageURL" alt="" >
+        </div>
+        <div class="w-[100%] lg:w-[50%] p-4 flex flex-col items-start lg:sticky">
+            <p class="text-[14px]">{{ product.brand }}</p>
+            <h2 class="text-grande font-semibold">{{ product.title }}</h2>
+            <p class="text-medio ">{{ product.price }}</p>
+            <p class="text-medio mt-5">{{ product.description }}</p>
+            <p class="text-piccolo mt-5">Disponibilità: {{ product.quantity }}</p>
+            <input class="mt-[50px] p-[5px] border-[1px] rounded-md border-black" type="number" v-model="quantity" min="1" :max="product.Quantity">
+            <button class="mt-[5px] bg-[#294f94ff] hover:bg-white hover:text-[#294f94ff] text-white text-medio font-medium lg:w-[400px] w-[200px] py-2 border border-[#294f94ff] border-[3px] rounded-md " @click="buttonClick">Aggiungi al carrello</button>
+        
+        </div>
+    </section>
+    
+    
+    
+    
+    
+    
+    
+    
+    
     <div v-if="product">
+        
         <button @click="buttonClick">| Add |</button>
         <input type="number" v-model="quantity" min="1" :max="product.Quantity">
 
@@ -55,8 +82,8 @@ export default {
         <p>Prezzo: {{ product.price }}</p>
         <p>Quantità: {{ product.quantity }}</p>
         <p>Marca: {{ product.brand }}</p>
-        <!-- Creare view per immagini e review -->
-        <img width="100px" v-for="imageURL in product.imageURLs" :src="imageURL" alt="" style="display: inline-block;">
+        <!-- Creare view per immagini e review  -->
+        <!-- <img width="100px" v-for="imageURL in product.imageURLs" :src="imageURL" alt="" style="display: inline-block;"> -->
         <p>reviews</p>
     </div>
 
